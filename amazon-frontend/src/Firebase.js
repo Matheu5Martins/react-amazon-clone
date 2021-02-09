da7e59d023aa61281b19062819c5165c3e3b3556
Matheu5Martins <dev.sincroni@gmail.com>
2021-02-09 15:56:04 -0300

    amazon-clonev1

---
+++ amazon-frontend/src/Firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyA2D0dSshFn1oA4rR_2MBbAyXcC4hMOMyw",
+    authDomain: "clone-cb084.firebaseapp.com",
+    projectId: "clone-cb084",
+    storageBucket: "clone-cb084.appspot.com",
+    messagingSenderId: "664022721890",
+    appId: "1:664022721890:web:d502479c13671e8be1b83b",
+    measurementId: "G-NPKTJ6358C"
+  }
